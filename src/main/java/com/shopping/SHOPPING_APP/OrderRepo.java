b351729e7b1208bb572ea2d8e0e9f825e27ce1a3
Vivek11N <viveknarayanan117@gamil.com>
2024-09-03 10:03:16 +0530

    done

---
+++ src/main/java/com/shopping/SHOPPING_APP/OrderRepo.java
@@ -0,0 +1,11 @@
+package com.shopping.SHOPPING_APP;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepo extends JpaRepository<Order, Integer> {
+
+
+
+
+
+}
---
+++ src/test/java/com/shopping/SHOPPING_APP/ShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.SHOPPING_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
